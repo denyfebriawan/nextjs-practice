ccb9d7ac1dafb1c733a520f41a3205f0fba90b76
denyfebriawan <denyfebriawan@gmail.com>
2024-10-21 17:12:18 +0700

    local env firebase setting

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -4,21 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCwp1-qkKDqkiSe4IpNB0j-3bdpP4PgMCo",
-  authDomain: "my-next-app-1-ff9fc.firebaseapp.com",
-  projectId: "my-next-app-1-ff9fc",
-  storageBucket: "my-next-app-1-ff9fc.appspot.com",
-  messagingSenderId: "98229210133",
-  appId: "1:98229210133:web:5ead234fa309c9c794921c",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
