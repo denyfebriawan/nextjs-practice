9211fe28d68d0d567376eaac8ff60310398a7ea3
denyfebriawan <denyfebriawan@gmail.com>
2024-10-22 14:07:39 +0700

    Separate View from pages

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -2,10 +2,16 @@
 import axiosClient from "@/lib/axiosClient";
 import { useEffect, useState } from "react";
 
+import ProductView from "@/view/Product";
+
 type Product = {
   id: number;
+  image: string;
   name: string;
   description: string;
+  category: string;
+  price: number;
+  rating: string;
 };
 
 type ProductType = {
@@ -16,39 +22,70 @@ type ProductType = {
 
 const Products = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      setLoading(true);
+      // setLoading(true);
       try {
         const response = await axiosClient.get<ProductType>("/products");
         setProducts(response.data.products);
       } catch (error: any) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
+        console.log(error);
       }
     };
     fetchProducts();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
   return (
-    <div>
-      <h1>Products</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <h3>{product.name}</h3>
-            <span>Description: {product.description}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ProductView products={products} />
+    // <div className={styles.product}>
+    //   <h1 className={styles.product__title}>Products</h1>
+    //   <div className={styles.product__content}>
+    //     {products.length > 0 ? (
+    //       <>
+    //         {products.map((product: Product) => (
+    //           <div className={styles.product__content__item} key={product.id}>
+    //             <img
+    //               className={styles.product__content__item__image}
+    //               src={product.image}
+    //               alt="bag"
+    //             />
+    //             <div className={styles.product__content__item__name}>
+    //               {product.name}
+    //             </div>
+    //             <div className={styles.product__content__item__category}>
+    //               {product.category}
+    //             </div>
+    //             <div className={styles.product__content__item__price}>
+    //               {product.price.toLocaleString("id-ID", {
+    //                 style: "currency",
+    //                 currency: "IDR",
+    //               })}
+    //             </div>
+    //             <div className={styles.product__content__item__rating}>
+    //               <span>rating : </span>
+    //               {product.rating}
+    //             </div>
+    //           </div>
+    //         ))}
+    //       </>
+    //     ) : (
+    //       <>
+    //         <div className={styles.product__content__skeleton}>
+    //           <div className={styles.product__content__skeleton__image}></div>
+    //           <div className={styles.product__content__skeleton__name}></div>
+    //           <div
+    //             className={styles.product__content__skeleton__category}
+    //           ></div>
+    //           <div className={styles.product__content__skeleton__price}></div>
+    //           <div className={styles.product__content__skeleton__rating}></div>
+    //         </div>
+    //       </>
+    //     )}
+    //   </div>
+    // </div>
   );
 };
 
---
+++ src/pages/products/server.tsx
@@ -0,0 +1,9 @@
+import Products from "@/pages/products/index";
+
+const ServerSideProduct = () => {
+  return (
+    <div>
+      <Products products={[]} />
+    </div>
+  );
+};
