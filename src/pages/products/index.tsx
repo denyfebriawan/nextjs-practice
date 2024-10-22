64c9f9789db278c6c9fe168d9f157f533c0d853a
denyfebriawan <denyfebriawan@gmail.com>
2024-10-22 14:22:34 +0700

    Server side rendering

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -40,52 +40,6 @@ const Products = () => {
 
   return (
     <ProductView products={products} />
-    // <div className={styles.product}>
-    //   <h1 className={styles.product__title}>Products</h1>
-    //   <div className={styles.product__content}>
-    //     {products.length > 0 ? (
-    //       <>
-    //         {products.map((product: Product) => (
-    //           <div className={styles.product__content__item} key={product.id}>
-    //             <img
-    //               className={styles.product__content__item__image}
-    //               src={product.image}
-    //               alt="bag"
-    //             />
-    //             <div className={styles.product__content__item__name}>
-    //               {product.name}
-    //             </div>
-    //             <div className={styles.product__content__item__category}>
-    //               {product.category}
-    //             </div>
-    //             <div className={styles.product__content__item__price}>
-    //               {product.price.toLocaleString("id-ID", {
-    //                 style: "currency",
-    //                 currency: "IDR",
-    //               })}
-    //             </div>
-    //             <div className={styles.product__content__item__rating}>
-    //               <span>rating : </span>
-    //               {product.rating}
-    //             </div>
-    //           </div>
-    //         ))}
-    //       </>
-    //     ) : (
-    //       <>
-    //         <div className={styles.product__content__skeleton}>
-    //           <div className={styles.product__content__skeleton__image}></div>
-    //           <div className={styles.product__content__skeleton__name}></div>
-    //           <div
-    //             className={styles.product__content__skeleton__category}
-    //           ></div>
-    //           <div className={styles.product__content__skeleton__price}></div>
-    //           <div className={styles.product__content__skeleton__rating}></div>
-    //         </div>
-    //       </>
-    //     )}
-    //   </div>
-    // </div>
   );
 };
 
--- src/pages/products/server.tsx
+++ src/pages/products/server.tsx
@@ -1,9 +1,31 @@
-import Products from "@/pages/products/index";
+import axiosClient from "@/lib/axiosClient";
+import ProductView from "@/view/Product";
 
-const ServerSideProduct = () => {
+type Product = {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  rating: string;
+};
+
+const ServerSideProduct = ({ products }: { products: Product[] }) => {
   return (
     <div>
-      <Products products={[]} />
+      <ProductView products={products} />
     </div>
   );
 };
+
+export default ServerSideProduct;
+
+export async function getServerSideProps() {
+  const res = await axiosClient.get("/products");
+  return {
+    props: {
+      products: res.data.products,
+    },
+  };
+}
